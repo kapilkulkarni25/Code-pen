37987eb04670cd80ba0754785f124325b0766833
kapilkulkarni25 <kapilkulkarni25@Github.com>
2020-10-28 20:20:43 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Editor from './Editor'
 
+
+
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+
+  const [html, setHtml] = useState('<h1>Hello</h1>')
+  const [css, setCss] = useState('body{background-color:wheat;}')
+  const [js, setJs] = useState('document.body.style.color = "white"', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        </html>
+      `)
+    }, 500)
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
   return (
     <>
       <div className="pane top-pane">
         <Editor language="xml" displayName="HTML"
           value={html}
           onChange={setHtml}
-        />
+        >Hello</Editor>
         <Editor language="css" displayName="CSS"
           value={css}
           onChange={setCss}
@@ -25,6 +44,7 @@ function App() {
       </div>
       <div className="pane">
         <iframe title="output"
+          srcDoc={srcDoc}
           sandbox="allow-scripts"
           frameBorder="0"
           width="100%"
